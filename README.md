8cf8ff4f9c9d458324f67db527bf2a2a07ca1dfd
Almir Jakubaitis <almir@icone3.com.br>
2020-06-03 20:52:44 -0300

    Challenge 03 completed :rocket:

---
+++ README.md
@@ -0,0 +1,41 @@
+<h1 align="center">GOSTACK 11 CHALLENGE 03</h1>
+<h1 align="center">ReactJS concepts :rocket:</h1>
+
+<img alt="GoStack" src="https://storage.googleapis.com/golden-wind/bootcamp-gostack/header-desafios.png" />
+
+
+
+## 💻 About the Project
+
+#### Challenge 03: ReactJS concepts
+
+
+<blockquote>
+This application connects to the API created earlier, allowing you to list portfolio repositories, add and delete new repositories.
+</blockquote>
+
+  
+
+ ## 💻 Technologies
+<h4>
+
+   - ReactJS
+
+   - Javascript
+
+
+
+
+### Aplication functions
+
+
+- List the repositories for your API: You should be able to create a list with the title field of all the repositories that are registered in your API.
+
+- Add a repository to your API: You must be able to add a new item to your API using a button with the text Add and, after creation, you must be able to display its name after registration.
+
+- Remove a repository from your API: For each item on your list, you must have a button with the text Remove which, when clicked, will call a function to remove that item from the list of your frontend and your API.
+
+## :memo: License
+
+This project is under the MIT license. 
+For more details visit: [LICENSE](.github/LICENSE.md).
--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from "react";
+import  { useState, useEffect } from 'react';
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      //console.log(response.data);
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
-  }
+
+      const response = await api.post('repositories', {
+        title: "Desafio 3 React JS",
+	      url: "https://github.com/Rocketseat/bootcamp-gostack-desafios",
+	      techs: ["Node.js","React JS", "React"]
+
+      });
+
+      const repository = response.data;
+      setRepositories([...repositories,repository]);
+
+  }  
 
   async function handleRemoveRepository(id) {
-    // TODO
+
+      await api.delete(`repositories/${id}`);
+
+      const respositoriesRest = repositories.filter(index => index.id !== id );
+      //console.log(respositoriesRest);
+
+      setRepositories([...respositoriesRest]);
+
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        { repositories.map(repository => 
+        
+         <li key={repository.id}>{repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button key={repository.id} onClick={() => handleRemoveRepository(`${repository.id}`)}>
             Remover
           </button>
         </li>
+        ) }
+
+
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
